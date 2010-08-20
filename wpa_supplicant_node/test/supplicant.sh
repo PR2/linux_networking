5d399df2b4d3281ea580a3c2180701abe72143e0
Blaise Gassend <blaise@willowgarage.com>
2010-08-20 06:28:11 +0000

    made the supplicant.sh script independent of where it is run from.

--- wpa_supplicant_node/test/supplicant.sh
+++ wpa_supplicant_node/test/supplicant.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 . `dirname $0`/config.sh
-./bin/wpa_supplicant_node -Dnl80211 -dddd -c ~/temp/net.conf -i $WLAN_TEST
+rosrun wpa_supplicant_node wpa_supplicant_node -Dnl80211 -dddd -c ~/temp/net.conf -i $WLAN_TEST
