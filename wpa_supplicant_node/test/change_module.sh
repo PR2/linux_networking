bcba3d1d53081e7d94543c5d19d13412620bd8f3
Blaise Gassend <blaise@willowgarage.com>
2010-08-20 23:43:59 +0000

    Added a script to rapidly swap between ralink modules

---
+++ wpa_supplicant_node/test/change_module.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+if lsmod |grep rt2800usb; then
+  rmmod rt2800usb
+fi
+
+if lsmdo |grep rt2870sta; then
+  rmmod rt2870sta
+fi
+
+sleep 1 
+
+case $1 in
+  sta)
+    MOD=rt2870sta
+    DEV=ra0
+  usb)
+    MOD=rt2800usb
+    DEV=wlan1
+  *)
+
+sleep 1
+modprobe $MOD
+ifconfig $DEV up
+echo
+iw dev $DEV scan freq 2437
+lsmod |grep $MOD
