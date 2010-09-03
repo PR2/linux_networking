dfa1e4a52266749d554eebc498b8007e218f9aca
Blaise Gassend <blaise@willowgarage.com>
2010-09-03 07:49:15 +0000

    Cleaned up the asynchronous runner module.

--- asmach/src/asmach/async.py
+++ asmach/src/asmach/async.py
@@ -8,102 +8,107 @@ class Return:
         self.value = value
         raise self
 
-indent = ""
-
-def Run(f):
-    f.next()
-    m = f.send
-    args = (None, )
-    global indent
-    while True:
-        try:
-            yield_val = m(*args)
-        except StopIteration:
-            raise Return(None)
-        else:
-            try:
-                m = f.send
-                if yield_val is None:
-                    args = yield
-                    args = (args, )
-                else:
-                    m = f.send
-                    print "indent"
-                    r = Run(yield_val)
-                    val = None
-                    while True:
-                        indent += "  "
-                        try:
-                            r.send(val)
-                            val = yield
-                        except Return, r:
-                            args = (r.value, )
-                            break
-                        except:
-                            r.throw(*sys.exc_info())
-                        finally:
-                            indent = indent[2:]
-            except:
-                m = f.throw
-                args = sys.exc_info()
-
-def run(f, name):
-    try:
-        print indent, "Starting", name
-        r = Run(f)
-        r.send(None)
+def start(f):
+    def run_internal(f):
+        entry = yield
+        f.next()
+        m = f.send
+        args = (None, )
         while True:
-            print indent, "Iterating on", name
-            #r.throw(Exception("Hahaha!"))
+            try:
+                yield_val = m(*args)
+            except StopIteration:
+                raise Return(None)
+            else:
+                try:
+                    m = f.send
+                    if yield_val is None:
+                        # We are yielding all the way back.
+                        args = yield
+                        args = (args, )
+                    else:
+                        # f is calling an asynchronous function.
+                        m = f.send
+                        r = run_internal(yield_val)
+                        r.next()
+                        val = (entry, )
+                        while True:
+                            try:
+                                r.send(val)
+                                val = yield
+                            except Return, r:
+                                args = (r.value, )
+                                break
+                except:
+                    m = f.throw
+                    args = list(sys.exc_info())
+                    # Remove misleading lines from the trace.
+                    try:
+                        args[2] = args[2].tb_next.tb_next
+                    except:
+                        pass
+    
+    r = run_internal(f)
+    r.next()
+    r.send(r)
+    return r
+
+def print_result(name, f = "<unnamed>"):
+    yield
+    try:
+        out = yield f
+        print name, "returned:", out
+    except:
+        print name, "had exception:"
+        tb = list(sys.exc_info())
+        tb[2] = tb[2].tb_next.tb_next
+        traceback.print_exception(*tb)
+
+def run(f, name = "<unnamed>"):
+    try:
+        r = start(print_result(name, f))
+        while True:
             r.send(None)
     except Return, r:
-        print indent, "Return value for %s:"%name, r.value
-    except:
-        print indent, "Had an exception in %s."%name
-        traceback.print_exc()
-        print indent, "Done with exception."
+        pass
 
 def f(a, b):
-    print indent, "f started"
     yield
-    print indent, "f running"
     Return(a + b)
 
+def g2():
+    yield
+    raise Exception("Booh!")
+
 def g(a, b):
-    print indent, "g1"
-    yield
-    print indent, "g2"
     yield
     yield
-    #raise Exception("Booh!")
-    print indent, "g3"
+    yield g2()
     Return(5)
 
 def h(a, b):
-    print indent, "h1"
     yield
-    print indent, "h2"
     out = yield (g(a, b))
-    print indent, "h3"
     Return(out)
 
 def i(a, b, c):
-    print indent, "i started"
     yield
-    print indent, "i running 1"
     out = yield (f(a, b))
-    print indent, "i running 2", out 
     out2 = f(out, c)
-    print indent, "i running 3", out2
     out = yield out2
-    print indent, "i returning"
     Return(out)
 
-run(f(1,2), "f")
-print
-run(g(1,2), "g")
-print
-run(h(1,2), "h")
-print
-run(i(1,2,3), "i")
+def demo(s):
+    run(eval(s), s)
 
+def main():
+    demo("f(1,2)")
+    print
+    demo("g(1,2)")
+    print
+    demo("h(1,2)")
+    print
+    demo("i(1,2,3)")
+
+if __name__ == "__main__":
+    main()
