f8776cb28f0b03713d6bb76287a05f85863f2c47
Blaise Gassend <blaise@willowgarage.com>
2010-09-09 06:55:05 +0000

    Built up the full stucture of the DHCP client. Just need to fill in packet sending and receiving, and configuration of parameters.

--- multi_interface_roam/src/multi_interface_roam/dhcp.py
+++ multi_interface_roam/src/multi_interface_roam/dhcp.py
@@ -7,26 +7,60 @@ from twisted.internet.defer import inlineCallbacks, returnValue
 from netlink_monitor import monitor, IFSTATE
 import async_helpers
 import l2socket
+import event
 
 class DhcpData:
     def __init__(self, iface):
         self.iface = iface
         self.socket = None
         self.link_addr_state = monitor.get_state_publisher(self.iface, IFSTATE.LINK_ADDR) 
+        self.error_event = event.Event()
+        self.error_timeout = 60
+        self.exp_backoff_min = 0.2
+        self.exp_backoff_max = 0.5
+        self.exp_backoff_timeout = 2
 
     def start_socket(self):
-        self.socket = async_helpers.ReadDescrEventStream(l2socket.L2Port, iface = self.iface, filter='udp and dst port 68')
+        if not self.socket or self.socket.port.fileno() == -1:
+            self.socket = async_helpers.ReadDescrEventStream(l2socket.L2Port, iface = self.iface, 
+                    filter='udp and dst port 68 and src port 67')
 
     def stop_socket(self):
         self.socket = None
 
-    def send_discover(self):
-        hwbytes = scapy.mac2str(self.hwaddr)
+class DhcpState(smach.State):
+    def __init__(self, *args, **kwargs):
+        smach.State.__init__(self, input_keys=['dhcp'], output_keys=['dhcp'], *args, **kwargs)
+
+
+
+class ExchangeRetryExponentialBackoff:
+    def init_retry_gen(self, ud):
+        self.cur_retry_max = ud.exp_backoff_min
+
+    def get_next_retry(self, ud):
+        interval = self.cur_retry_max * random.uniform(0.5, 1)
+        self.cur_retry_max = min(ud.exp_backoff_max, 2 * self.cur_retry_max)
+
+
+
+class ExchangeRetryHalve:
+    def init_retry_gen(self, ud):
+        pass
+
+    def get_next_retry(self, ud):
+        interval = 60 + (ud.timeout_time[self.type] - time.time()) / 2
+
+
+        
+class ExchangeDiscover:
+    def send(self, ud):
+        hwbytes = scapy.mac2str(self.ud.hwaddr)
         pkt = (
-              scapy.Ether(src=self.hwaddr, dst='ff:ff:ff:ff:ff:ff')/
+              scapy.Ether(src=self.ud.hwaddr, dst='ff:ff:ff:ff:ff:ff')/
               scapy.IP(src='0.0.0.0', dst='255.255.255.255')/
               scapy.UDP(sport=68, dport=67)/
-              scapy.BOOTP(chaddr=[hwbytes])/
+              scapy.BOOTP(chaddr=[hwbytes], xid=self.xid)/
               scapy.DHCP(options=[
                   ("message-type", "discover"),
                   "end",
@@ -34,121 +68,167 @@ class DhcpData:
               )
         self.socket.port.send(str(pkt))
 
-class DhcpState(smach.State):
-    def __init__(self, *args, **kwargs):
-        smach.State.__init__(self, input_keys=['dhcp'], output_keys=['dhcp'], *args, **kwargs)
+    def validate(self, pkt, ud):
 
-class Linkless(DhcpState):
+
+
+class ExchangeRequest:
+    def send(self, ud):
+        hwbytes = scapy.mac2str(self.ud.hwaddr)
+        pkt = (
+              scapy.Ether(src=self.ud.hwaddr, dst='ff:ff:ff:ff:ff:ff')/
+              scapy.IP(src='0.0.0.0', dst='255.255.255.255')/
+              scapy.UDP(sport=68, dport=67)/
+              scapy.BOOTP(chaddr=[hwbytes], xid=self.xid)/
+              scapy.DHCP(options=[
+                  ("message-type", "request"),
+                  "end",
+                  ])
+              )
+        self.socket.port.send(str(pkt))
+
+    def validate(self, pkt, ud):
+        Set ud.timeout_time
+
+
+
+class NoLink(DhcpState):
     def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
+        DhcpState.__init__(self, outcomes=['bound', 'init', 'init_reboot'])
  
     @inlineCallbacks
     def execute_async(self, ud):
-        pass
-
-
-
-class InitReboot(DhcpState):
-    def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
- 
-    @inlineCallbacks
-    def execute_async(self, ud):
-        pass
-
-
-
-class Rebooting(DhcpState):
-    def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
- 
-    @inlineCallbacks
-    def execute_async(self, ud):
-        pass
+        ud.dhcp.hwaddr = yield async_helpers.wait_for_state(ud.dhcp.link_addr_state, False, True)
+        returnValue('init')
 
 
 
 class Init(DhcpState):
     def __init__(self):
-        DhcpState.__init__(self, outcomes=['done'])
+        DhcpState.__init__(self, outcomes=['done', 'nolink'])
 
     @inlineCallbacks 
     def execute_async(self, ud):
-        ud.dhcp.hwaddr = yield async_helpers.wait_for_state(ud.dhcp.link_addr_state, False, True)
+        ud.dhcp.start_socket()
         returnValue('done')
 
 
 
-class Selecting(DhcpState):
-    def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
+class Exchange(DhcpState):
+    def __init__(self, mode):
+        DhcpState.__init__(self, outcomes=['success', 'fail', 'nolink'])
+        self.mode = mode
  
     @inlineCallbacks
     def execute_async(self, ud):
-        pass
+        # Parameters that will depend on the state.
+        self.init_retry_gen(ud)
+        timeout = Timeout()
+
+        # Make sure we aren't discarding incoming dhcp packets.
+        ud.dhcp.socket.set_discard(False)
+
+        # Generate an xid for the exchange.
+        self.xid = random.randint(0, 0xFFFF)
+
+        while True:
+            # Send a request packet
+            try:
+                self.send_time = time.time()
+                self.send(ud)
+                FIXME
+                ### discover/request
+                ### 
+            except:
+                returnValue('fail')
+
+            # How long to wait before retry
+            self.get_next_retry(ud)
+
+            while True:
+                # Wait for an event
+                events = yield async_helpers.select(
+                        StateCondition(ud.dhcp.link_addr_state, False, False), 
+                        ud.dhcp.socket, 
+                        Timeout(interval),
+                        timeout)
+
+                if 0 in events: # Lost link
+                    returnValue('nolink')
+    
+                if 1 in events: # Got packet
+                    pkt = Ether(ud.dhcp.socket.recv())
+                    pkt = pkt.payload # IP
+                    pkt = pkt.payload # UDP
+                    pkt = pkt.payload # BOOTP
+
+                    # Check xid
+                    # Check this is a response packet
+                    # Check it is destined to us
+                    valid = self.validate(ud, pkt)
+                    if valid:
+                        returnValue(valid)
+    
+                if 2 in events:
+                    break
+    
+                if 3 in events:
+                    returnValue('fail')
 
 
 
-class Requesting(DhcpState):
-    def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
- 
-    @inlineCallbacks
-    def execute_async(self, ud):
-        ud.dhcp.start_socket()
-        ud.dhcp.send_discover()
-        selectout = yield async_helpers.select(ud.dhcp.socket)
-        pkt = ud.dhcp.socket.recv()
-        ud.dhcp.stop_socket()
-        returnValue('got_replies')
-
-
-
-class Bound(DhcpState):
-    def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
- 
-    @inlineCallbacks
-    def execute_async(self, ud):
-        pass
-
-
-
-class Renewing(DhcpState):
-    def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
- 
-    @inlineCallbacks
-    def execute_async(self, ud):
-        pass
-
-
-
-class Rebinding(DhcpState):
-    def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
- 
-    @inlineCallbacks
-    def execute_async(self, ud):
-        pass
+class Rebooting  (Exchange, ExchangeRetryExponentialBackoff, ExchangeRequest ): type = "Rebooting"
+class Selecting  (Exchange, ExchangeRetryExponentialBackoff, ExchangeDiscover): type = "Selecting"
+class Requesting (Exchange, ExchangeRetryExponentialBackoff, ExchangeRequest ): type = "Requesting"
+class Renewing   (Exchange, ExchangeRetryHalve,              ExchangeRequest ): type = "Renewing"
+class Rebinding  (Exchange, ExchangeRetryHalve,              ExchangeRequest ): type = "Rebinding"
 
 
 
 class Error(DhcpState):
     def __init__(self):
-        DhcpState.__init__(self, outcomes=['got_replies', 'no_replies'])
+        DhcpState.__init__(self, outcomes=['done', 'nolink'])
  
     @inlineCallbacks
     def execute_async(self, ud):
-        pass
+        ud.dhcp.error_event.trigger()
+        ud.dhcp.socket.set_discard(True)
+        events = yield async_helpers.switch({
+            StateCondition(ud.dhcp.link_addr_state, False, False) : lambda _: returnValue('nolink'), 
+            Timeout(ud.dhcp.error_timeout)                        : lambda _: returnValue('timeout'),
+            })
+
+
+
+class Bound(DhcpState):
+    def __init__(self):
+        DhcpState.__init__(self, outcomes=['timeout', 'nolink'])
+ 
+    @inlineCallbacks
+    def execute_async(self, ud):
+        ud.dhcp.socket.set_discard(True)
+        FIXME Actually set the address
+        events = yield async_helpers.switch({
+            StateCondition(ud.dhcp.link_addr_state, False, False) : lambda _: returnValue('nolink'), 
+            Timeout(ud.timeout_time['Bound'] - time.time())       : lambda _: returnValue('timeout'),
+            })
 
 
 
 def start_dhcp(iface):
-    sm = smach.StateMachine(outcomes=['exit'], input_keys=['dhcp'])
+    sm = smach.StateMachine(outcomes=[], input_keys=['dhcp'])
+    smadd = smach.StateMachine.add
     with sm:
-        smach.StateMachine.add('INIT', Init(), transitions = {'done':'REQUESTING'})
-        smach.StateMachine.add('REQUESTING', Requesting(), transitions = {'got_replies':'INIT', 'no_replies':'INIT'})
+        smadd('NOLINK',      NoLink(),      transitions = {'bound'  :'BOUND',      'init':'INIT',      'init_reboot':'INIT_REBOOT'})
+        smadd('INIT_REBOOT', Init(),        transitions = {'done'   :'REBOOTING',                      'nolink'     :'NOLINK'})
+        smadd('REBOOTING',   Rebooting(),   transitions = {'success':'BOUND',      'fail':'INIT',      'nolink'     :'NOLINK'})
+        smadd('INIT',        Init(),        transitions = {'done'   :'SELECTING',                      'nolink'     :'NOLINK'})
+        smadd('SELECTING',   Selecting(),   transitions = {'success':'REQUESTING', 'fail':'ERROR',     'nolink'     :'NOLINK'})
+        smadd('REQUESTING',  Requesting(),  transitions = {'success':'BOUND',      'fail':'ERROR',     'nolink'     :'NOLINK'})
+        smadd('BOUND',       Bound(),       transitions = {'timeout':'RENEWING',                       'nolink'     :'NOLINK'})
+        smadd('RENEWING',    Renewing(),    transitions = {'success':'BOUND',      'fail':'REBINDING', 'nolink'     :'NOLINK'})
+        smadd('REBINDING',   Rebinding(),   transitions = {'success':'BOUND',      'fail':'INIT',      'nolink'     :'NOLINK'})
+        smadd('ERROR',       Error(),       transitions = {'done'   :'INIT',                           'nolink'     :'NOLINK'})
 
     ud = smach.UserData()
     ud.dhcp = DhcpData(iface)
