c879a500830a2902464e26ed9ed7eaae9d1f9251
Blaise Gassend <blaise@willowgarage.com>
2010-10-27 18:46:17 +0000

    Previous commit had not removed sigmask.

--- multi_interface_roam/src/multi_interface_roam/sigmask_build.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-
-from distutils.core import setup, Extension
-
-setup(name = "sigmask",
-        version = "1.0",
-        ext_modules = [Extension("sigmask", ["sigmask.c"])])
